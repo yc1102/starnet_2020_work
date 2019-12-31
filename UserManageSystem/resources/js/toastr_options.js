2632b31181aa4a70e4b37b3e2a38ce5978cc4204
yc1102 <59396855+yc1102@users.noreply.github.com>
2020-01-01 01:14:15 +0800

    Add files via upload

---
+++ UserManageSystem/resources/js/toastr_options.js
@@ -0,0 +1,12 @@
+// toastr配置
+toastr.options = {
+    positionClass: "toast-top-center",
+    showDuration: "200",
+    hideDuration: "600",
+    timeOut: "2000",
+    extendedTimeOut: "1000",
+    showEasing: "swing",
+    hideEasing: "linear",
+    showMethod: "fadeIn",
+    hideMethod: "fadeOut"
+};
